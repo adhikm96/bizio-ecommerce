9aa347324f91b72f8ea887a2f195c9e07b0a2f5e
dheerajjaynath.v <dheerajjaynath.vishwakarma@digitalairtech.com>
2024-06-24 20:39:31 +0530

    updates

--- internal/model/orderItem.go
+++ internal/model/orderItem.go
@@ -5,7 +5,7 @@ type OrderItem struct {
 	OrderID          uint           `json:"order_id" gorm:"index:idx_order_items_order_id"`
 	Order            Order          `gorm:"constraint:OnDelete:CASCADE"`
 	ProductVariantID uint           `json:"variant_id" gorm:"index:idx_order_items_variant_id"`
-	ProductVariant   ProductVariant `gorm:"foreignKey:variant_id"`
+	ProductVariant   ProductVariant `gorm:"foreignKey:ProductVariantID"`
 	Quantity         uint           `json:"quantity" gorm:"not null"`
 	Price            float64        `json:"price" gorm:"type:decimal(10, 2); not null"`
 }
