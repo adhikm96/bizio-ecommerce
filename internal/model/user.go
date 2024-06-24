5045e76278b16b5466f1a4e6e8b26aad6b5b4225
Amit <amit.k@digitalairtech.com>
2024-06-24 13:39:33 +0530

    model updates

--- internal/model/user.go
+++ internal/model/user.go
@@ -2,7 +2,7 @@ package model
 
 type User struct {
 	BaseEntity
-	Username     string `json:"username" gorm:"size:100, not null"`
-	Email        string `json:"email" gorm:"size:100, not null"`
-	PasswordHash string `json:"password_hash" gorm:"size:255, not null"`
+	Username     string `json:"username" gorm:"type:varchar(100);size:100;uniqueIndex;not null"`
+	Email        string `json:"email" gorm:"type:varchar(100);size:100;uniqueIndex;not null"`
+	PasswordHash string `json:"password_hash" gorm:"type:varchar(255);size:255;not null"`
 }
